83c0dfac69a4777b68ac5d96c74b2e9beb503b31
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-07-26 13:45:03 -0700

    Add web routes
    - These will be for small admin interface

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/login', ['as' => 'admin.login', 'uses' => 'AdminController@loginShow']);
+Route::post('admin/login', 'AdminController@loginPost');
+
+Route::group(['middleware' => 'auth:admin'], function ($router) {
+
+    Route::get('admin', ['as' => 'admin.index', 'uses' => 'AdminController@index']);
+    Route::get('admin/add', 'AdminController@addClient');
+
+});
