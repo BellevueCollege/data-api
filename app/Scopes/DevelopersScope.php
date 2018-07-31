bc4dc8bd1f08ba96672f5e639cf342e26ee5f1ea
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-07-31 13:30:40 -0700

    Clean up scope code

--- app/Scopes/DevelopersScope.php
+++ app/Scopes/DevelopersScope.php
@@ -4,7 +4,6 @@ namespace App\Scopes;
 
 use Adldap\Query\Builder;
 use Adldap\Laravel\Scopes\ScopeInterface;
-use Illuminate\Support\Facades\Log;
 
 class DevelopersScope implements ScopeInterface
 {
@@ -18,9 +17,9 @@ class DevelopersScope implements ScopeInterface
     public function apply(Builder $query)
     {
         // The distinguished name of our LDAP group.
-        $basedn = config('adldap.connections.default.connection_settings.base_dn'); //config('adldap')['connections']['default']['connection_settings']['base_dn']; //env('ADLDAP_BASEDN');
+        $basedn = config('adldap.connections.default.connection_settings.base_dn');
         $devs = sprintf("CN=Developers,OU=Security Groups,OU=General Distribution - Security Groups,OU=AllGroups,%s", $basedn);
-        Log::info("devs: " . $devs);
+
         $query->whereMemberOf($devs);
     }
 }
