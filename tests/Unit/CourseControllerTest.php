77234a202ab26f2d9b0db042991d3f2f216ef732
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-10-08 14:33:06 -0700

    Add/update single course info unit test

--- tests/Unit/CourseControllerTest.php
+++ tests/Unit/CourseControllerTest.php
@@ -12,17 +12,27 @@ class CourseControllerTest extends TestCase
         //route not used as it doesn't make sense
         //$this->get('v1/courses')->assertEquals('Hi. Do not try to see that many courses.');
         //$this->assertEquals(200, $this->response->status());
-    }
-    
-    public function testGetCourse() {
-        //route not currently used so functionality not complete
-        
-        //$this->get('v1/courses/ACCT 101');
-        
-        //$this->assertEquals(200, $this->response->status());
-        
     }*/
     
+    public function testGetCourse() {
+        //test a valid course number
+        //$this->get('/api/v1/course/ACCT 101');
+        $response = $this->get('/api/v1/course/ACCT 101')
+                        ->assertJsonFragment([
+                            /*'areas' => [],*/
+                            'subject' => 'ACCT',
+                            'courseNumber' => '101'
+                        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    public function testGetCourseBadCourse() {
+        //should return empty object so don't need to check json, just need to make sure it doesn't error
+        $response = $this->get('/api/v1/course/XYZ 100');
+        
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
     public function testGetMultipleCourses() {
         //valid course numbers
         $response = $this->get('/api/v1/courses/multiple', [ 'courses[]' => 'ACCT 101', 'courses[]' => 'BTS 293', 'courses[]' => 'ADFIT 020' ]);
