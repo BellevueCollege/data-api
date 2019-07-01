01ce00e32eb70bd164dfe6b765cbb23734e936fe
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-07-01 11:15:07 -0700

    👌 IMPROVE: Web Routing File Structure

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,24 @@
 |
 */
 
+Route::domain(config('dataapi.api_internal_domain'))->group( function( $router ) {
+    Route::name('admin.')->group(function () {
+
+        Route::get('admin/login', 'AdminController@loginShow')->name('login');
+        Route::post('admin/login', 'AdminController@loginPost');
+
+        Route::middleware(['auth:admin'])->group(function () {
+
+            Route::get('admin', 'AdminController@index')->name('index');
+            Route::get('admin/client/add', 'AdminController@addClientShow')->name('client.add');
+            Route::post('admin/client/add', 'AdminController@addClientPost')->name('client.add');
+            Route::get('admin/client/{id}/delete', 'AdminController@deleteClient')->name('client.delete');
+            Route::get('admin/logout', 'AdminController@logout')->name('logout');
+
+        });
+    });
+});
+
 Route::get('/', function () {
     return ''; //Return blank for main URL
-});
-
-Route::group(['domain' => config('dataapi.api_internal_domain')], function ($router) {
-
-    Route::get('admin/login', ['as' => 'admin.login', 'uses' => 'AdminController@loginShow']);
-    Route::post('admin/login', 'AdminController@loginPost');
-
-});
-
-Route::group(['domain' => config('dataapi.api_internal_domain'), 'middleware' => 'auth:admin'], function ($router) {
-
-    Route::get('admin', ['as' => 'admin.index', 'uses' => 'AdminController@index']);
-    Route::get('admin/client/add', ['as' => 'admin.client.add', 'uses' => 'AdminController@addClientShow']);
-    Route::post('admin/client/add', ['as' => 'admin.client.add', 'uses' => 'AdminController@addClientPost']);
-    Route::get('admin/client/{id}/delete', ['as' => 'admin.client.delete', 'uses' => 'AdminController@deleteClient']);
-    Route::get('admin/logout', ['as' => 'admin.logout', 'uses' => 'AdminController@logout']);
-
 });
