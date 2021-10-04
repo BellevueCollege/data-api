69bd2bc946804da11ac4b552bddddce40224e826
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2021-10-04 14:32:09 -0700

    👌 IMPROVE: Add Days to Section Info

--- app/Http/Transformers/SectionTransformer.php
+++ app/Http/Transformers/SectionTransformer.php
@@ -4,25 +4,37 @@ use App\Models\Section;
 use League\Fractal\Resource\Collection;
 use League\Fractal\TransformerAbstract;
 
-class SectionTransformer extends TransformerAbstract {
+class SectionTransformer extends TransformerAbstract
+{
 
     /**
-    * Fractal transformer that defines how data for a section 
+    * Fractal transformer that defines how data for a section
     * should be output in the API.
     **/
 
     public function transform(Section $sec)
     {
+        /**
+         * Get asssociated day
+         */
+        $days = $sec->day()->first();
+        if (isset($day->Title)) {
+            $days = $day->Title;
+        } else {
+            $days = "";
+        }
+        //dd($days);
+
         return [
-            'id'           	=> $sec->ClassID,
-            'section' => $sec->Section,
+            'id'            => $sec->ClassID,
+            'section'       => $sec->Section,
             'itemNumber'    => $sec->ItemNumber,
-			'instructor'	=> $sec->InstructorName,
+            'instructor'    => $sec->InstructorName,
             'beginDate'     => $sec->getFormattedDate($sec->StartDate),
             'endDate'       => $sec->getFormattedDate($sec->EndDate),
-			'room'			=> $sec->location,
+            'room'          => $sec->location,
+            'days'          => $days,
             'schedule'      => trim($sec->schedule),
         ];
     }
-	
 }
