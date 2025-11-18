67552cb30d2757df80b4fce2d361461adb3b0654
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2025-11-18 15:42:48 -0800

    🐛 FIX: Sessions and Cache Table Migrations

--- database/migrations/2025_11_18_215051_create_cache_table.php
+++ database/migrations/2025_11_18_215051_create_cache_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
+        Schema::connection('da')->create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->integer('expiration');
         });
 
-        Schema::create('cache_locks', function (Blueprint $table) {
+        Schema::connection('da')->create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
             $table->integer('expiration');
@@ -29,7 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+        Schema::connection('da')->dropIfExists('cache');
+        Schema::connection('da')->dropIfExists('cache_locks');
     }
 };
--- database/migrations/2025_11_18_215135_create_sessions_table.php
+++ database/migrations/2025_11_18_215135_create_sessions_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::connection('da')->create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignUuid('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
+        Schema::connection('da')->dropIfExists('sessions');
     }
 };
