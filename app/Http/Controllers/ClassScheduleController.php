c060fd0dd6987e79707de0738b8b49978bec0331
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2025-11-21 14:18:35 -0800

    👌 IMPROVE: ClassSchedule to use Resource and Collection

--- app/Http/Controllers/ClassScheduleController.php
+++ app/Http/Controllers/ClassScheduleController.php
@@ -5,13 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ClassSchedule;
 use Illuminate\Http\Request;
 use App\Exceptions\MissingParameterException;
-use League\Fractal\Manager;
-use League\Fractal\Resource\Item;
-use League\Fractal\Resource\Collection;
-use App\Http\Transformers\ClassScheduleTransformer;
-use App\Http\Serializers\CustomDataArraySerializer;
-use DB;
-
+use App\Http\Resources\ClassScheduleCollection;
 
 class ClassScheduleController extends ApiController{
 
@@ -22,17 +16,13 @@ class ClassScheduleController extends ApiController{
     * Status: inactive
     * No route, not yet serialized
     **/
-    public function getClassSchedules($psclassid, Request $request){
-        $schedules = ClassSchedule::where('PSClassID', '=', $psclassid)->get();
-
-        $data = $schedules;
-        if ( !is_null($schedules) ) {
-            $collection = new Collection($schedules, new ClassScheduleTransformer, self::WRAPPER);
-
-            $fractal = new Manager;
-            $fractal->setSerializer(new CustomDataArraySerializer);
-            $data = $fractal->createData($collection)->toArray();
+    public function getClassSchedules($psclassid, Request $request)
+    {
+        try {
+            $schedules = ClassSchedule::where('PSClassID', '=', $psclassid)->get();
+            return new ClassScheduleCollection($schedules);
+        } catch (\Exception $e) {
+            return response()->json(['classSchedules' => []], 404);
         }
-        return $this->respond($data);
     }
 }
